726c31b55a6543805f28fd4ba6a7b31704087cc4
nuwanranawaka <115425855+nuwanranawaka@users.noreply.github.com>
2022-10-19 17:12:38 +0530

    add to api

--- routes/vision.js
+++ routes/vision.js
@@ -1,17 +1,43 @@
 var express = require('express');
 var router = express.Router();
 
-router.post('/classify', function(req, res, next) {
+const { Rekognition} = require("aws-sdk");
+
+router.post('/classify', async function(req, res, next) {
   // DON'T return the hardcoded response after implementing the backend
   let response = ["shoe", "red", "nike"];
 
   // Your code starts here //
+  try {
 
+    const rekognition = new Rekognition({
+      region: "ap-southeast-1", // like us-east-1
+    });
+      
+    await rekognition. detectLabels({
+      Image: {
+        Bytes: req.files.file.data
+      }
+    }, (err, data) => {
+      if (err) {
+        console.log(err, err . stack) ;
+      }else {
+        for (let Label of data.Labels) {
+          labelArray.push(Label.Name);
+        }
+      }
+    });
+
+    res.json({
+      "labels": labelArray
+    });
+    
+  } catch (error) {
+    console.log(error);
+  }
   // Your code ends here //
 
-  res.json({
-    "labels": response
-  });
+  
 });
 
 module.exports = router;
