d46de5bdc7c3a861e009aefb6127395ddafde80d
nuwanranawaka <115425855+nuwanranawaka@users.noreply.github.com>
2022-10-20 00:47:30 +0530

    new update

--- routes/vision.js
+++ routes/vision.js
@@ -1,39 +1,51 @@
 var express = require('express');
 var router = express.Router();
 
-const { Rekognition} = require("aws-sdk");
+const AWS = require("aws-sdk");
 
 router.post('/classify', async function(req, res, next) {
   // DON'T return the hardcoded response after implementing the backend
   let response = ["shoe", "red", "nike"];
-
+  let labelArray = [];
   // Your code starts here //
   try {
 
-    const rekognition = new Rekognition({
-      region: "ap-southeast-1", // like us-east-1
+    const config = new AWS.Config({
+      accessKeyId: "AKIARAR74F5B2ZJFROOU",
+      secretAccessKey: "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",
+      region: "ap-southeast-1"
+    }) 
+
+    const rekognition = new AWS.Rekognition({
+      region: "ap-southeast-1",
     });
       
-    await rekognition. detectLabels({
+    await rekognition.detectLabels({
       Image: {
         Bytes: req.files.file.data
       }
     }, (err, data) => {
       if (err) {
         console.log(err, err . stack) ;
+        res.status(500).json({
+          "error": "Error"
+        });
       }else {
+        console.log(data.Labels);
         for (let Label of data.Labels) {
           labelArray.push(Label.Name);
         }
+        res.json({
+          "labels": labelArray
+        });
       }
-    });
-
-    res.json({
-      "labels": labelArray
     });
     
   } catch (error) {
     console.log(error);
+    res.status(500).json({
+      "error": "Error"
+    });
   }
   // Your code ends here //
 });
