b9f592851ea5842d8bdae093863188df21ea1751
Asjath Husni <asjath.husni@gmail.com>
2023-10-31 22:02:50 +0530

    feat: vision backend updated

--- routes/vision.js
+++ routes/vision.js
@@ -1,17 +1,52 @@
-var express = require('express');
+var express = require("express");
 var router = express.Router();
 
-router.post('/classify', function(req, res, next) {
+router.post("/classify", function (req, res, next) {
   // DON'T return the hardcoded response after implementing the backend
   let response = ["shoe", "red", "nike"];
 
   // Your code starts here //
 
-  // Your code ends here //
+  const { fromIni } = require("@aws-sdk/credential-providers");
+  const {
+    RekognitionClient,
+    DetectLabelsCommand,
+  } = require("@aws-sdk/client-rekognition");
 
-  res.json({
-    "labels": response
+  const uplodedFile = req.files.file;
+
+  const client = new RekognitionClient({
+    // set region as ap-southeast-1(singapore)
+    region: "ap-southeast-1",
+
+    // different kind of credentials configurations options can be found from here : https://docs.aws.amazon.com/AWSJavaScriptSDK/v3/latest/Package/-aws-sdk-credential-providers/#fromenv
+    credentials: fromIni({ profile: "work-account" }),
   });
+
+  const params = {
+    Image: {
+      Bytes: uplodedFile.data,
+    },
+    MaxLabels: 10,
+    MinConfidence: 90,
+  };
+
+  let awsResponse = client.send(new DetectLabelsCommand(params));
+  awsResponse
+    .then((dataResponse) => {
+      const mappedLabels = dataResponse.Labels.map((labels) => labels.Name);
+      res.json({
+        labels: mappedLabels,
+      });
+    })
+    .catch((err) => {
+      const error = {
+        error: "Unable to process the request",
+      };
+      res.status(500).send(error);
+    });
+
+  // Your code ends here //
 });
 
 module.exports = router;
